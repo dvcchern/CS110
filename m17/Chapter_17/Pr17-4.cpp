8ccf97e500bfaea58226da7378270ed9f2509cd7
Tony Chern <35832318+dvcchern@users.noreply.github.com>
2020-05-25 23:39:43 -0700

    05252020

---
+++ m17/Chapter_17/Pr17-4.cpp
@@ -0,0 +1,24 @@
+// This program demonstrates the Insertion Sort 
+// using the insertNode function (class method.)
+#include <iostream>
+#include "NumberList.cpp" // For AWS only inclusion
+#include "NumberList.h"
+using namespace std;
+
+int main()
+{
+   // Define a NumberList object.
+   NumberList list;
+
+   // Build the list with some values.
+   list.appendNode(2.5);
+   list.appendNode(7.9);
+   list.appendNode(12.6);
+
+   // Insert a node in the middle of the list.
+   list.insertNode(10.5);
+
+   // Dispay the list
+   list.displayList();
+   return 0;
+}

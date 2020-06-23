395657f4c565512db9cd892ed9be91596d8143eb
Tony Chern <35832318+dvcchern@users.noreply.github.com>
2020-06-23 00:20:11 -0700

    06222020

---
+++ m01/ex1C.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "*******************************\n"
+         << "Programmer: Tony Chern" << endl
+         << "123 Main Street\n"
+         << "Pleasant Hill, CA\n"
+         << "Telephone: 925-555-1212\n"
+         << "********************************";
+};
